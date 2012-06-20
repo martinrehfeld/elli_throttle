3f5caa52ea8ac818aa75504d0a3113d5635b345a
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2012-06-20 12:06:28 +0200

    track request count per hour/day

--- src/elli_throttle_server.erl
+++ src/elli_throttle_server.erl
@@ -9,7 +9,12 @@
          terminate/2, code_change/3]).
 
 
--record(state, {}).
+-record(state,    {startup_time}).
+-record(counters, {hour,
+                   hourly_requests,
+                   day,
+                   daily_requests}).
+
 
 %%%===================================================================
 %%% API
@@ -29,14 +34,16 @@ request(Id) ->
 %%%===================================================================
 
 init([]) ->
-    {ok, #state{}}.
+    {ok, #state{startup_time = current_timestamp()}}.
 
 
 handle_call(_, _From, State) ->
     {reply, ok, State}.
 
 
-handle_cast({request, _Id}, State) ->
+handle_cast({request, Id}, #state{startup_time = StartupTime} = State) ->
+    Key = {elli_throttle, Id},
+    put(Key, update_counters(get(Key), StartupTime)),
     {noreply, State};
 
 handle_cast(_, State) ->
@@ -58,3 +65,44 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+update_counters(undefined, StartupTime) ->
+    #counters{hour = current_hour(StartupTime),
+              hourly_requests = 1,
+              day = current_day(StartupTime),
+              daily_requests = 1};
+
+update_counters(#counters{hour = TrackedHour,
+                          hourly_requests = HourlyRequests,
+                          day = TrackedDay,
+                          daily_requests = DailyRequests}, StartupTime) ->
+
+    {NewHour, NewHourlyRequests} =
+        updated_hourly(current_hour(StartupTime), TrackedHour, HourlyRequests),
+    {NewDay, NewDailyRequests} =
+        updated_daily(current_day(StartupTime), TrackedDay, DailyRequests),
+    #counters{hour = NewHour,
+              hourly_requests = NewHourlyRequests,
+              day = NewDay,
+              daily_requests = NewDailyRequests}.
+
+
+updated_hourly(CurrentHour, TrackedHour, Count)
+  when CurrentHour =:= TrackedHour ->
+    {TrackedHour, Count + 1};
+updated_hourly(CurrentHour, _TrackedHour, _Count) ->
+    {CurrentHour, 1}.
+
+updated_daily(CurrentDay, TrackedDay, Count)
+  when CurrentDay =:= TrackedDay ->
+    {TrackedDay, Count + 1};
+updated_daily(CurrentDay, _TrackedDay, _Count) ->
+    {CurrentDay, 1}.
+
+
+current_hour(StartupTime) -> (current_timestamp() - StartupTime) div 3600.
+current_day(StartupTime)  -> (current_timestamp() - StartupTime) div 86400.
+
+current_timestamp() ->
+    {MegaSecs, Secs, _MicroSecs} = os:timestamp(),
+    MegaSecs * 1000000 + Secs.
